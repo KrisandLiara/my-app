2c74a712d0df9353a27b129b04d1fcc0282c8c51
KrisandLiara <123495766+KrisandLiara@users.noreply.github.com>
2023-07-28 18:01:49 +0100

    replaced import statment to require

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import axios from 'axios';
+const axios = require('axios');
 
 test('renders learn react link', () => {
   render(<App />);
