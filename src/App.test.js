057322528eef8e2ba54d406a4d053786c089a1d1
KrisandLiara <123495766+KrisandLiara@users.noreply.github.com>
2023-07-31 13:02:23 +0100

    eddited test file  4

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,35 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
-const axios = require('axios');
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import axios from 'axios';
+import { MemoryRouter } from 'react-router-dom';
+import ChatLayout from './ChatLayout';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 // This line tells Jest to replace axios with our mock function
 jest.mock('axios');
+
+test('sends message when send button is clicked', async () => {
+  const message = 'test message';
+  const responseMessage = 'response message';
+  
+  axios.post.mockResolvedValue({ data: { message: responseMessage } });
+
+  render(
+    <MemoryRouter>
+      <ChatLayout />
+    </MemoryRouter>
+  );
+  
+  const input = screen.getByPlaceholderText('Type your message...');
+  fireEvent.change(input, { target: { value: message } });
+
+  const button = screen.getByText('Send');
+  fireEvent.click(button);
+
+  // This line checks that axios.post was called with the expected arguments
+  expect(axios.post).toHaveBeenCalledWith('/api/v1/chat', { prompt: message });
+
+  await screen.findByText(responseMessage);
+});
 
 test('mocks axios.post call', async () => {
   // Here we define what the mock function should return
