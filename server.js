812e5a7a3acb29a8a4c0fb3dfce76188484f0adc
KrisandLiara <123495766+KrisandLiara@users.noreply.github.com>
2023-08-08 20:23:23 +0100

    Connecting session do database

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-//server.js
 // 1. Importing required modules
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const cors = require('cors');
-const path = require('path');
-const app = express();
+require('dotenv').config(); // Load environment variables from a .env file into process.env
+const express = require('express'); // Import Express, a web application framework for Node.js
+const session = require('express-session'); // Import express-session, a middleware for handling sessions
+const cors = require('cors'); // Import CORS, a middleware for enabling Cross-Origin Resource Sharing
+const path = require('path'); // Import the path module, which provides utilities for working with file and directory paths
+const app = express(); // Create an Express application
+
+// 2. Connecting to Cosmos DB
+const { CosmosClient } = require("@azure/cosmos"); // Import the CosmosClient class from the @azure/cosmos package
+const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING); // Create a new CosmosClient instance with the connection string from the environment variables
+console.log('Connected to Cosmos DB'); // Log a message to indicate that the connection was successful
+const database = client.database('appdb'); // Get a reference to the database
+const container = database.container('chatSessions'); // Get a reference to the container
 
 // Enable CORS
-app.use(cors());
+app.use(cors()); // Use the CORS middleware
 
 // Use express-session for session management
 app.use(session({
@@ -19,30 +25,34 @@ app.use(session({
 }));
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json());
+app.use(express.json()); // Use the express.json middleware to parse JSON request bodies
 
-// routes
-const chatRoute = require('./chatroutes/chatRoute');
-app.use('/api/v1/chat', chatRoute);  // Note the version number in the route
+// Define routes
+const chatRoute = require('./chatroutes/chatRoute')(container); // Import the chatRoute module
+app.use('/api/v1/chat', chatRoute);  // Use the chatRoute middleware for requests to /api/v1/chat
 
-const valdisChatRoute = require('./chatroutes/valdisChatRoute');
-app.use('/api/v1/chat/valdis', valdisChatRoute);
+const valdisChatRoute = require('./chatroutes/valdisChatRoute'); // Import the valdisChatRoute module
+app.use('/api/v1/chat/valdis', valdisChatRoute); // Use the valdisChatRoute middleware for requests to /api/v1/chat/valdis
 
-const port = process.env.PORT || 80; // Use the PORT environment variable if available, or use 5000 as default
-app.listen(port, () => {
+// Start the server
+const port = process.env.PORT || 5000; // Use the PORT environment variable if available, or use 5000 as default
+app.listen(port, () => { // Start the server and listen for requests on the specified port
   console.log(`Server is running on port ${port}`);
   console.log('Open the following link in your browser: http://localhost:' + port);
 });
+
 // Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: err.toString() });
+app.use((err, req, res, next) => { // Use a middleware function to handle errors
+  console.error(err.stack); // Log the error stack trace
+  console.error('Global error handler caught an error:', err.stack); // Log the error stack trace
+  res.status(500).json({ error: err.toString() }); // Send a response with the error message and a 500 status code
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './build')));
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') { // If the app is running in production mode
+  app.use(express.static(path.join(__dirname, './build'))); // Serve static files from the build directory
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+  app.get('*', (req, res) => { // Handle all GET requests
+    res.sendFile(path.resolve(__dirname, './build', 'index.html')); // Send the index.html file
   });
 }
--- src/components/ChatLayout.js
+++ src/components/ChatLayout.js
@@ -1,51 +1,84 @@
-//ChatLayout.js
-import React, { useState } from 'react';
+// Import necessary libraries and modules
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './chat.css';
 
+// Define the ChatLayout component
 function ChatLayout() {
-  const [userInput, setUserInput] = useState("");
-  const [chatHistory, setChatHistory] = useState([]);
+  // Declare state variables
+  const [userInput, setUserInput] = useState(""); // The user's current input
+  const [chatHistory, setChatHistory] = useState([]); // The history of the chat
+  const [sessionId, setSessionId] = useState(null); // The id of the current chat session
 
+  // Define a function to auto-expand the textarea
   const autoExpand = (textarea) => {
     textarea.style.height = 'auto';
     textarea.style.height = textarea.scrollHeight + 'px';
   }
 
+  // Define a function to handle changes to the user's input
   const handleUserInput = (event) => {
-    setUserInput(event.target.value);
-    autoExpand(event.target);
+    setUserInput(event.target.value); // Update the userInput state variable with the new input
+    autoExpand(event.target); // Auto-expand the textarea
   }
 
-  const handleSendMessage = async () => {
-    if (userInput.trim() !== '') {
-      setChatHistory((prevMessages) => [...prevMessages, { text: userInput, from: 'user' }]);
-      setUserInput('');
+  // Define a function to start a new chat session
+  const startChat = async () => {
+    console.log('Sending request to /start');
+    const response = await axios.post('/api/v1/chat/start'); // Send a request to the /start endpoint
+    const { sessionId } = response.data; // Extract the session id from the server's response
+    setSessionId(sessionId); // Set the sessionId state variable with the new session id
+  };
+
+// Define a function to handle sending a message
+const handleSendMessage = async () => {
+  console.log('Send button clicked. User input:', userInput); // Log when the Send button is clicked
+
+  if (userInput.trim() !== '') {
+    // Start a new session if one hasn't been started yet
+    if (!sessionId) {
       try {
-        const response = await axios.post('/api/v1/chat', { prompt: userInput });
-        const { data } = response;
-        console.log('Response from server: ', data);
-        if (data.message) {
-          setChatHistory((prevMessages) => [...prevMessages, { text: data.message, from: 'bot' }]);
-          setUserInput('');
-          console.log('Updated messages: ', chatHistory);
-        } else {
-          console.error('No message in response data:', data);
-        }
+        await startChat();
       } catch (error) {
-        console.error('Error sending chat request:', error);
+        console.error('Error starting chat session:', error);
+        return;  // Return early if an error occurred
       }
     }
-  };
 
+    setChatHistory((prevMessages) => [...prevMessages, { text: userInput, from: 'user' }]);
+    setUserInput('');
 
+    try {
+      const response = await axios.post('/api/v1/chat', { prompt: userInput, sessionId });
+      const { data } = response;
+      console.log('Response from server: ', data);
+      if (data.message) {
+        setChatHistory((prevMessages) => [...prevMessages, { text: data.message, from: 'bot' }]);
+        setUserInput('');
+        console.log('Updated messages: ', chatHistory);
+      } else {
+        console.error('No message in response data:', data);
+      }
+    } catch (error) {
+      console.error('Error sending chat request:', error);
+    }
+  }
+};
+
+  // Define a function to handle the Enter key press
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault();
-      handleSendMessage();
+    if (event.key === 'Enter' && !event.shiftKey) { // If the Enter key is pressed without the Shift key
+      event.preventDefault(); // Prevent the default action (submitting the form)
+      handleSendMessage(); // Send the user's message
     }
   };
 
+  // Use the useEffect hook to start a new chat session when the component mounts
+  useEffect(() => {
+    startChat();
+  }, []);
+
+  // Render the component
   return (
     <div className="chat-layout">
       <div className="chat-box">
@@ -54,7 +87,7 @@ function ChatLayout() {
             <span>{message.from === 'user' ? 'User: ' : 'Bot: '}</span>
             <span>{message.text}</span>
           </div>
-          ))}
+        ))}
       </div>
       <div className="chat-input">
         <textarea className="expanding-textarea" value={userInput} onChange={handleUserInput} onKeyPress={handleKeyPress} placeholder="Type your message here..."></textarea>
@@ -64,4 +97,5 @@ function ChatLayout() {
   );
 }
 
+// Export the ChatLayout component
 export default ChatLayout;
